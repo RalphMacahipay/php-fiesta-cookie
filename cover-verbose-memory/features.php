a3bd99b479d1799d66429cb1025e82ee3a7ff78d
GevelaL <you@example.com>
2022-02-24 07:11:09 -0800

    inc directory done

---
+++ cover-verbose-memory/features.php
@@ -0,0 +1,49 @@
+
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Features</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link href="../../dist/css/bootstrap.min.css" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+  <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+       <?php require_once 'inc/header.php' ?>
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Features</h1>
+        <p class="lead">Handsome</p>
+        <p class="lead">Gentlemen</p>
+        <p class="lead">Grind All Day</p>
+        <p class="lead">No to Weakness, Build up Strength</p>
+        <p class="lead">Ralph</p>
+        
+        <p class="lead">
+        </p>
+      </main>
+      <?php require_once 'inc/footer.php' ?>
+    </div>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+
+</body></html>
---
+++ cover-verbose-memory/inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<header class="masthead mb-auto">
+  <div class="inner">
+    <h3 class="masthead-brand">RalPh</h3>
+    <nav class="nav nav-masthead justify-content-center">
+      <a class="nav-link" href="index.php">Home</a>
+      <a class="nav-link" href="features.php">Features</a>
+      <a class="nav-link" href="Contacts.php">Contact</a>
+    </nav>
+  </div>
+</header>
+</body>
+</html>
